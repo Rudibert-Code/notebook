1c1f2d6147effcb3a8dcbad3b9ebd8f91e173bee
Bjoern <onoofelogmail.com>
2026-03-04 17:18:28 +0100

    added the function to slice elements out of array by click & reload edited array

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,47 @@
 
 let myNotes = ["hi"," banana", " apfel", " nö"];
 let newNotes;
+let noteAdded = false;
+let addedNoteIndex;
 
-function renderNotes(newNoteIndex){
-        if (newNoteIndex > 0) {
-            newNoteIndex--
-            const note = myNotes[newNoteIndex];
-            document.getElementById('notes-location').innerHTML += `<p><a href="#" onclick="deleteNote(${newNoteIndex})">${note}</a></p>`;   
-        } else {
-            for (let i = 0; i < myNotes.length; i++) {
-                const note = myNotes[i];
-                document.getElementById('notes-location').innerHTML += `<p><a href="#" onclick="deleteNote(${i})">${note}</a></p>`;
-            } 
-        }
+function initFunction(){
+    document.addEventListener("keyup", processKeyEntered);
+}
+
+function processKeyEntered(key){
+    if (key.key == "Enter") {
+        submitNotes()
+    }
+}
+
+function renderNotes(){
+    initFunction();
+    if (noteAdded == true) {
+        const note = myNotes[addedNoteIndex];
+        document.getElementById('notes-location').innerHTML += `<p><a href="#" onclick="deleteNote(${addedNoteIndex})">${note}</a></p>`; 
+        noteAdded = false; 
+    } else {
+        for (let i = 0; i < myNotes.length; i++) {
+            const note = myNotes[i];
+            document.getElementById('notes-location').innerHTML += `<p><a href="#" onclick="deleteNote(${i})">${note}</a></p>`;
+        } 
+    }
 }
 
 function submitNotes(){
     newNotes = "";
     newNotes = document.getElementById('newNotes').value;
     myNotes.push(newNotes);
-    let newIndex = myNotes.length;
-    renderNotes(newIndex);
+    addedNoteIndex = myNotes.length;
+    addedNoteIndex--
+    noteAdded = true;
+    document.getElementById('newNotes').value = "";
+    renderNotes();
 }
 
 function deleteNote(elementLocation){
     myNotes.splice(elementLocation, 1);
+    document.getElementById('notes-location').innerHTML = ""; 
     renderNotes();
 }
 
