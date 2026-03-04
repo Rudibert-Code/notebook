b44cef7e54bfe6a01f79c1d98474861997f3beae
Bjoern <onoofelogmail.com>
2026-03-04 14:43:39 +0100

    - added text input - added submit button - added function to push new notes from input area to array

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
 // Display notes
+
+let myNotes = ["hi"," banana", " apfel", " nö"];
+let newNotes;
+
+function renderNotes(){
+    document.getElementById('notes-location').innerHTML = (`<p>${myNotes}</p>`)
+}
+
+function submitNotes(){
+    newNotes = "";
+    newNotes = document.getElementById('newNotes').value;
+    myNotes.push = newNotes;
+    renderNotes();
+}
+
+
+
 // Add notes
 // Delete notes
 // Archive notes
